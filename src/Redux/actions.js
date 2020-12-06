1ff8a974b9c0ccecb25709f205df5cdffbee75e1
Kevin Valias <Kevbodyworks@gmail.com>
2020-12-06 14:53:34 -0600

    New relative paths

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -17,10 +17,10 @@ export const removeRegion = (index) => {
 //     value: bool,
 //   };
 // };
-
+// after building new actions make sure to make relative paths//
 const loginUser = (User) => {
   return function (dispatch) {
-    fetch("http://localhost:4000/auth/login", {
+    fetch("/auth/login", {
       method: "POST",
       body: JSON.stringify(User),
       headers: {
@@ -57,7 +57,7 @@ const userAdded = (data) => {
 };
 const setUser = (User) => {
   return function (dispatch) {
-    fetch("http://localhost:4000/auth/signup", {
+    fetch("/auth/signup", {
       method: "POST",
       body: JSON.stringify(User),
       headers: {
