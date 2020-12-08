ebf4fca523bcff090c340ba09f4a6f1400f4ebf1
Kevin Valias <Kevbodyworks@gmail.com>
2020-12-07 19:13:55 -0600

    FInal pushes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const { logger } = require("./middleware");
 const app = express();
 
 const port = process.env.PORT || 4000;
-app.use(express.static("build"));
+app.use(express.static("client/build"));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
